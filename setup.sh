8e498ac2fce659ef282f12b3b71437372b718111
LiYouBioinfo <100756226+LiYouBioinfo@users.noreply.github.com>
2025-08-05 17:56:37 +0800

    ForDockerBuild

--- setup.sh
+++ setup.sh
@@ -1,11 +1,11 @@
 ### Download AF2 parameters
-cd ./src/AF2
-mkdir params
-cd params
-wget https://storage.googleapis.com/alphafold/alphafold_params_2021-07-14.tar
-tar -xf alphafold_params_2021-07-14.tar
-rm alphafold_params_2021-07-14.tar
-cd ../../../
+#cd ./src/AF2
+#mkdir params
+#cd params
+#wget https://storage.googleapis.com/alphafold/alphafold_params_2021-07-14.tar
+#tar -xf alphafold_params_2021-07-14.tar
+#rm alphafold_params_2021-07-14.tar
+#cd ../../../
 
 #Python packages
 conda env create -f environment.yml
@@ -22,8 +22,8 @@ cmake -DCMAKE_INSTALL_PREFIX=. ..
 make -j 4 && make install
 cd ../..
 
-cd data
+#cd data
 ### Download uniclust30_2018_08
-wget http://wwwuser.gwdg.de/~compbiol/uniclust/2018_08/uniclust30_2018_08_hhsuite.tar.gz --no-check-certificate
-tar -zxvf uniclust30_2018_08_hhsuite.tar.gz
-rm uniclust30_2018_08_hhsuite.tar.gz
+#wget http://wwwuser.gwdg.de/~compbiol/uniclust/2018_08/uniclust30_2018_08_hhsuite.tar.gz --no-check-certificate
+#tar -zxvf uniclust30_2018_08_hhsuite.tar.gz
+#rm uniclust30_2018_08_hhsuite.tar.gz
